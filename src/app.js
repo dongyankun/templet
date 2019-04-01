1d4e238552c8fe8f7c63f68cd33cda79649f10a0
dongyankun <1366455248@qq.com>
2019-04-01 15:12:42 +0800

    1

--- src/app.js
+++ src/app.js
@@ -8,6 +8,7 @@ import axios from 'axios'
 
 //Vue.use(ElementUI);
 
+
 Vue.prototype.baseUrl = 'http://data.xinxueshuo.cn:80/nsi-1.0'
 //Vue.prototype.baseUrl = 'http://192.168.0.40:8080/nsi-1.0'
 Vue.prototype.$axios = axios
